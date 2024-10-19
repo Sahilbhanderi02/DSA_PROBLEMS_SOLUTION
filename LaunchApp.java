abe3636d89b33f6fd350d258f8a7dcff6560b702
Sahilbhanderi02 <sahilbhanderi02@gmail.com>
2024-10-19 16:08:05 +0530

    2nd commit

--- LaunchApp.java
+++ LaunchApp.java
@@ -3,5 +3,7 @@ class Launch
 	public static void main(String[]args)
 	{
 		System.out.println("Hello LeetCode");
+		System.out.println("thodak chanegs karya bhai );
+		System.out.println("thodak chanegs karya bhai biji var 2344 );	
 	}
 }
